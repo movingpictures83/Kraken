d74d082b0f83319d6ff04e95ca36cb913a639391
Trevor Cickovski <tcickovs@delphi.cs.fiu.edu>
2022-12-21 13:52:06 -0500

    Can do single or paired

--- KrakenPlugin.R
+++ KrakenPlugin.R
@@ -7,7 +7,9 @@ input <- function(infile) {
    parameters <<- readParameters(infile)
    samplex <<- parameters['samplex', 2]
    fq1 <<- parameters['fq1', 2]
+   if ("fq2" %in% rownames(parameters)){
    fq2 <<- parameters['fq2', 2]
+   }
    ncbi_blast_path <<- parameters['ncbi_blast_path', 2]
    Kraken2Uniq_path <<- parameters['Kraken2Uniq', 2]
    kraken_database_path <<- parameters['kraken_database_path', 2]
@@ -22,6 +24,7 @@ output <- function(outfile) {
 str = "";
 print(c(ncbi_blast_path))
 # run Kraken
+   if ("fq2" %in% rownames(parameters)){
 str = paste0(str,
              'export PATH=$PATH:', c(ncbi_blast_path), ' \n\n',
              Kraken2Uniq_path, ' \\\n',
@@ -36,6 +39,21 @@ str = paste0(str,
              paste0(fq1, ' \\\n'),
              paste0(fq2, '\n\n')
 )
+   }
+   else{
+str = paste0(str,
+             'export PATH=$PATH:', c(ncbi_blast_path), ' \n\n',
+             Kraken2Uniq_path, ' \\\n',
+             '--db ', kraken_database_path, ' \\\n',
+             '--threads 24 \\\n',
+             '--use-names \\\n',
+             '--report-minimizer-data \\\n',
+             '--classified-out ', paste0(out_path, samplex), '#.fq', ' \\\n',
+             '--output ', paste0(out_path, samplex, '.kraken.output.txt'), ' \\\n',
+             '--report ', paste0(out_path, samplex, '.kraken.report.txt'), ' \\\n',#,
+             paste0(fq1, ' \\\n')
+)
+   }
 print(str)
 system(str)
 }
